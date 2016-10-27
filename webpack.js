40daa65631b5bf2d33f6aa82c53a41ca6eb5a2e4
nuintun <nuintun@qq.com>
2016-10-27 15:44:23 +0800

    change structure

---
+++ webpack.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+  //页面入口文件配置
+  entry: {
+    'qrcode.all': 'src/index.js',
+    'qrcode.encode': 'src/encode/qrcode.js',
+    'qrcode.decode': 'src/decode/qrdecode.js'
+  },
+  //入口文件输出配置
+  output: {
+    path: 'dist/',
+    filename: '[name].js'
+  },
+  //其它解决方案配置
+  resolve: {
+    alias: {
+      jquery: 'jquery'
+    }
+  }
+};
