b30b327b5df5028962ceaffc6434d040e566a525
nuintun <nuintun@qq.com>
2016-10-27 16:28:17 +0800

    update files

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
-  //页面入口文件配置
+  // 页面入口文件配置
   entry: {
-    'qrcode.all': './src/index.js',
+    'qrcode.all': './src/index.es6',
     'qrcode.encode': './src/encode/qrcode.js',
     'qrcode.decode': './src/decode/qrdecode.js'
   },
-  //入口文件输出配置
+  // 入口文件输出配置
   output: {
     path: 'dist/',
     filename: '[name].js'
   },
-  //其它解决方案配置
+  // 模块配置
+  module: {
+    loaders: [
+      {
+        test: path.join(__dirname, 'es6'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  // 其它解决方案配置
   resolve: {
     alias: {
       jquery: 'jquery'
