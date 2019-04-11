620d7e83e4b9bd29e0efa0d2e10d53c154416083
nuintun <nuintun@qq.com>
2019-04-11 17:44:44 +0800

    :recycle: Refactoring code

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,3 @@
 // core/qrcode
 export { default as QRCodeReader } from './core/qrcode/QRCodeReader';
 export { default as QRCodeWriter } from './core/qrcode/QRCodeWriter';
-export { default as QRCodeEncoder } from './core/qrcode/encoder/Encoder';
-export { default as QRCodeEncoderQRCode } from './core/qrcode/encoder/QRCode';
-export { default as QRCodeDecoderErrorCorrectionLevel } from './core/qrcode/decoder/ErrorCorrectionLevel';
