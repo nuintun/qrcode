06b1ea5a4c463709b65cdb5d7474ab307d19f357
nuintun <nuintun@qq.com>
2019-04-13 08:12:17 +0800

    :recycle: rewrite

--- src/image/GIFImage.ts
+++ src/image/GIFImage.ts
@@ -253,15 +253,16 @@ export default class GIFImage {
 
     this.write(output);
 
-    let url: string = 'data:image/gif;base64,';
     const bytes: number[] = Base64.encode(output.toByteArray());
+
+    output.close();
+
     const length: number = bytes.length;
+    let url: string = 'data:image/gif;base64,';
 
     for (let i: number = 0; i < length; i++) {
       url += String.fromCharCode(bytes[i]);
     }
-
-    output.close();
 
     return url;
   }
