954f735a17106367f81980c0ad2ddb2bbfc18dac
nuintun <nuintun@qq.com>
2019-04-13 09:54:52 +0800

    :fire: Clean unused variable

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const { Encoder, ErrorCorrectLevel, QRKanji, QR8BitByte } = require('./es5');
+const { Encoder, QRKanji, QR8BitByte } = require('./es5');
 
 const qrcode = new Encoder();
 
 qrcode.addData('世界你好！\n');
 qrcode.addData(new QR8BitByte('hello world !\n'));
 qrcode.addData(new QRKanji('こんにちは世界！'));
-
 qrcode.make();
-console.log(qrcode.getVersion())
+
+console.log(qrcode.getVersion());
 console.log(qrcode.toDataURL());
