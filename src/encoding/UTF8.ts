62a24972d8de89d7237982e1b541799adfbf3bdc
nuintun <nuintun@qq.com>
2019-04-14 10:01:55 +0800

    :recycle: Rewrite

--- src/encoding/UTF8.ts
+++ src/encoding/UTF8.ts
@@ -1,43 +1,39 @@
 /**
  * @module UTF8
- * @author elhigu
  * @author nuintun
- * @author Kazuhiko Arase
  */
 
 /**
  * @function UTF8
  * @param str
- * @see http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
+ * @see https://github.com/google/closure-library/blob/master/closure/goog/crypt/crypt.js
  */
 export default function UTF8(str: string): number[] {
-  const utf8: number[] = [];
-  const length: number = str.length;
+  let pos: number = 0;
+  const output: number[] = [];
 
-  for (let i: number = 0; i < length; i++) {
-    let charcode: number = str.charCodeAt(i);
+  for (let i: number = 0; i < str.length; i++) {
+    let code: number = str.charCodeAt(i);
 
-    if (charcode < 0x80) {
-      utf8.push(charcode);
-    } else if (charcode < 0x800) {
-      utf8.push(0xc0 | (charcode >> 6), 0x80 | (charcode & 0x3f));
-    } else if (charcode < 0xd800 || charcode >= 0xe000) {
-      utf8.push(0xe0 | (charcode >> 12), 0x80 | ((charcode >> 6) & 0x3f), 0x80 | (charcode & 0x3f));
+    if (code < 128) {
+      output[pos++] = code;
+    } else if (code < 2048) {
+      output[pos++] = (code >> 6) | 192;
+      output[pos++] = (code & 63) | 128;
+    } else if ((code & 0xfc00) == 0xd800 && i + 1 < str.length && (str.charCodeAt(i + 1) & 0xfc00) == 0xdc00) {
+      // Surrogate Pair
+      code = 0x10000 + ((code & 0x03ff) << 10) + (str.charCodeAt(++i) & 0x03ff);
+
+      output[pos++] = (code >> 18) | 240;
+      output[pos++] = ((code >> 12) & 63) | 128;
+      output[pos++] = ((code >> 6) & 63) | 128;
+      output[pos++] = (code & 63) | 128;
     } else {
-      // surrogate pair
-      // UTF-16 encodes 0x10000-0x10FFFF by
-      // subtracting 0x10000 and splitting the
-      // 20 bits of 0x0-0xFFFFF into two halves
-      charcode = 0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i++) & 0x3ff));
-
-      utf8.push(
-        0xf0 | (charcode >> 18),
-        0x80 | ((charcode >> 12) & 0x3f),
-        0x80 | ((charcode >> 6) & 0x3f),
-        0x80 | (charcode & 0x3f)
-      );
+      output[pos++] = (code >> 12) | 224;
+      output[pos++] = ((code >> 6) & 63) | 128;
+      output[pos++] = (code & 63) | 128;
     }
   }
 
-  return utf8;
+  return output;
 }
