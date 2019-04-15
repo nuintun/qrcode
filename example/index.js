5ac48f8acb63e94e40d1317fd76ab258bf2eb3b6
nuintun <nuintun@qq.com>
2019-04-15 09:37:56 +0800

    :recycle: Update files

---
+++ example/index.js
@@ -0,0 +1,10 @@
+const { Encoder, QRKanji, QRByte } = require('../es5');
+
+const qrcode = new Encoder();
+
+qrcode.write('世界你好！\n');
+qrcode.write(new QRByte('hello world !\n'));
+qrcode.write(new QRKanji('こんにちは世界！'));
+qrcode.make();
+
+console.log(qrcode.toDataURL());
