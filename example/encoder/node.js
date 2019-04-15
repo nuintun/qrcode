186a8a930fa5adac604d942b01f9ce404d6c75e3
nuintun <nuintun@qq.com>
2019-04-15 15:54:51 +0800

    :art: Update example

--- example/encoder/node.js
+++ example/encoder/node.js
@@ -2,7 +2,7 @@ const { Encoder, QRKanji, QRByte } = require('../../es5');
 
 const qrcode = new Encoder();
 
-qrcode.write('世界你好\n');
+qrcode.write('你好世界\n');
 qrcode.write(new QRByte('hello world\n'));
 qrcode.write(new QRKanji('こんにちは世界'));
 qrcode.make();
