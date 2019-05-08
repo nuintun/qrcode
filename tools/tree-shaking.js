0bd3e9c565b0cb033c8b292b80decac67c87e8d1
nuintun <nuintun@qq.com>
2019-05-08 11:56:02 +0800

    :bug: Fixed typescript Tree-shaking bug

---
+++ tools/tree-shaking.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const { parse } = require('@babel/parser');
+const { default: traverse } = require('@babel/traverse');
+const { default: generate } = require('@babel/generator');
+
+function read(src, callback) {
+  fs.readdir(src, { withFileTypes: true }, (error, files) => {
+    if (!error) {
+      files.forEach(file => {
+        const path = `${src}/${file.name}`;
+
+        if (file.isDirectory()) {
+          read(path, callback);
+        } else if (file.isFile() && /\.js$/i.test(file.name)) {
+          callback(path);
+        }
+      });
+    }
+  });
+}
+
+const src = process.argv[2];
+
+if (src === 'es5' || src === 'esnext') {
+  read(src, src => {
+    fs.readFile(src, (error, buffer) => {
+      if (!error) {
+        const ast = parse(buffer.toString(), { sourceType: 'module' });
+
+        traverse(ast, {
+          CallExpression(path) {
+            if (Array.isArray(path.node.leadingComments)) {
+              path.node.leadingComments.forEach(node => {
+                if (node.value === '* @class ') {
+                  node.value = '#__PURE__';
+                }
+              });
+            }
+          }
+        });
+
+        const { code } = generate(ast, { retainLines: true });
+
+        fs.writeFile(src, code, () => {});
+      }
+    });
+  });
+}
