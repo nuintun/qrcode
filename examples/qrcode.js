a3bc710ee07e179f38daa3c1a4e8203ac2612be8
nuintun <nuintun@qq.com>
2021-02-26 11:09:55 +0800

    :tada: Update files

--- examples/qrcode.js
+++ examples/qrcode.js
@@ -25,7 +25,7 @@
   /**
    * @readonly
    */
-
+  exports.Mode = void 0;
   (function (Mode) {
     Mode[(Mode['Terminator'] = 0)] = 'Terminator';
     Mode[(Mode['Numeric'] = 1)] = 'Numeric';
@@ -68,6 +68,8 @@
   };
 
   function __extends(d, b) {
+    if (typeof b !== 'function' && b !== null)
+      throw new TypeError('Class extends value ' + String(b) + ' is not a constructor or null');
     extendStatics(d, b);
     function __() {
       this.constructor = d;
@@ -238,27 +240,6 @@
     };
     return QRByte;
   })(QRData);
-
-  /**
-   * @module ErrorCorrectionLevel
-   * @author nuintun
-   * @author Cosmo Wolfe
-   * @author Kazuhiko Arase
-   */
-  /**
-   * @readonly
-   */
-
-  (function (ErrorCorrectionLevel) {
-    // 7%
-    ErrorCorrectionLevel[(ErrorCorrectionLevel['L'] = 1)] = 'L';
-    // 15%
-    ErrorCorrectionLevel[(ErrorCorrectionLevel['M'] = 0)] = 'M';
-    // 25%
-    ErrorCorrectionLevel[(ErrorCorrectionLevel['Q'] = 3)] = 'Q';
-    // 30%
-    ErrorCorrectionLevel[(ErrorCorrectionLevel['H'] = 2)] = 'H';
-  })(exports.ErrorCorrectionLevel || (exports.ErrorCorrectionLevel = {}));
 
   /**
    * @module QRMath
@@ -560,6 +541,27 @@
       applyMaskPenaltyRule4(qrcode)
     );
   }
+
+  /**
+   * @module ErrorCorrectionLevel
+   * @author nuintun
+   * @author Cosmo Wolfe
+   * @author Kazuhiko Arase
+   */
+  /**
+   * @readonly
+   */
+  exports.ErrorCorrectionLevel = void 0;
+  (function (ErrorCorrectionLevel) {
+    // 7%
+    ErrorCorrectionLevel[(ErrorCorrectionLevel['L'] = 1)] = 'L';
+    // 15%
+    ErrorCorrectionLevel[(ErrorCorrectionLevel['M'] = 0)] = 'M';
+    // 25%
+    ErrorCorrectionLevel[(ErrorCorrectionLevel['Q'] = 3)] = 'Q';
+    // 30%
+    ErrorCorrectionLevel[(ErrorCorrectionLevel['H'] = 2)] = 'H';
+  })(exports.ErrorCorrectionLevel || (exports.ErrorCorrectionLevel = {}));
 
   /**
    * @module RSBlock
@@ -2159,6 +2161,72 @@
   }
 
   /**
+   * @module GenericGF
+   * @author nuintun
+   * @author Cosmo Wolfe
+   * @license https://raw.githubusercontent.com/cozmo/jsQR/master/LICENSE
+   */
+  function addOrSubtractGF(a, b) {
+    return a ^ b;
+  }
+  var GenericGF = /*#__PURE__*/ (function () {
+    function GenericGF(primitive, size, generatorBase) {
+      this.primitive = primitive;
+      this.size = size;
+      this.generatorBase = generatorBase;
+      this.expTable = [];
+      this.logTable = [];
+      var x = 1;
+      for (var i = 0; i < this.size; i++) {
+        this.logTable[i] = 0;
+        this.expTable[i] = x;
+        x = x * 2;
+        if (x >= this.size) {
+          x = (x ^ this.primitive) & (this.size - 1);
+        }
+      }
+      for (var i = 0; i < this.size - 1; i++) {
+        this.logTable[this.expTable[i]] = i;
+      }
+      this.zero = new GenericGFPoly(this, Uint8ClampedArray.from([0]));
+      this.one = new GenericGFPoly(this, Uint8ClampedArray.from([1]));
+    }
+    GenericGF.prototype.multiply = function (a, b) {
+      if (a === 0 || b === 0) {
+        return 0;
+      }
+      return this.expTable[(this.logTable[a] + this.logTable[b]) % (this.size - 1)];
+    };
+    GenericGF.prototype.inverse = function (a) {
+      if (a === 0) {
+        throw new Error("can't invert 0");
+      }
+      return this.expTable[this.size - this.logTable[a] - 1];
+    };
+    GenericGF.prototype.buildMonomial = function (degree, coefficient) {
+      if (degree < 0) {
+        throw new Error('invalid monomial degree less than 0');
+      }
+      if (coefficient === 0) {
+        return this.zero;
+      }
+      var coefficients = new Uint8ClampedArray(degree + 1);
+      coefficients[0] = coefficient;
+      return new GenericGFPoly(this, coefficients);
+    };
+    GenericGF.prototype.log = function (a) {
+      if (a === 0) {
+        throw new Error("can't take log(0)");
+      }
+      return this.logTable[a];
+    };
+    GenericGF.prototype.exp = function (a) {
+      return this.expTable[a];
+    };
+    return GenericGF;
+  })();
+
+  /**
    * @module GenericGFPoly
    * @author nuintun
    * @author Cosmo Wolfe
@@ -2287,72 +2355,6 @@
       return result;
     };
     return GenericGFPoly;
-  })();
-
-  /**
-   * @module GenericGF
-   * @author nuintun
-   * @author Cosmo Wolfe
-   * @license https://raw.githubusercontent.com/cozmo/jsQR/master/LICENSE
-   */
-  function addOrSubtractGF(a, b) {
-    return a ^ b;
-  }
-  var GenericGF = /*#__PURE__*/ (function () {
-    function GenericGF(primitive, size, generatorBase) {
-      this.primitive = primitive;
-      this.size = size;
-      this.generatorBase = generatorBase;
-      this.expTable = [];
-      this.logTable = [];
-      var x = 1;
-      for (var i = 0; i < this.size; i++) {
-        this.logTable[i] = 0;
-        this.expTable[i] = x;
-        x = x * 2;
-        if (x >= this.size) {
-          x = (x ^ this.primitive) & (this.size - 1);
-        }
-      }
-      for (var i = 0; i < this.size - 1; i++) {
-        this.logTable[this.expTable[i]] = i;
-      }
-      this.zero = new GenericGFPoly(this, Uint8ClampedArray.from([0]));
-      this.one = new GenericGFPoly(this, Uint8ClampedArray.from([1]));
-    }
-    GenericGF.prototype.multiply = function (a, b) {
-      if (a === 0 || b === 0) {
-        return 0;
-      }
-      return this.expTable[(this.logTable[a] + this.logTable[b]) % (this.size - 1)];
-    };
-    GenericGF.prototype.inverse = function (a) {
-      if (a === 0) {
-        throw new Error("can't invert 0");
-      }
-      return this.expTable[this.size - this.logTable[a] - 1];
-    };
-    GenericGF.prototype.buildMonomial = function (degree, coefficient) {
-      if (degree < 0) {
-        throw new Error('invalid monomial degree less than 0');
-      }
-      if (coefficient === 0) {
-        return this.zero;
-      }
-      var coefficients = new Uint8ClampedArray(degree + 1);
-      coefficients[0] = coefficient;
-      return new GenericGFPoly(this, coefficients);
-    };
-    GenericGF.prototype.log = function (a) {
-      if (a === 0) {
-        throw new Error("can't take log(0)");
-      }
-      return this.logTable[a];
-    };
-    GenericGF.prototype.exp = function (a) {
-      return this.expTable[a];
-    };
-    return GenericGF;
   })();
 
   /**
