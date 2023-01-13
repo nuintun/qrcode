d1e068539dc68e652ea0cc4f73353b793a55c8ae
nuintun <nuintun@qq.com>
2023-01-13 10:41:06 +0800

    chore: Update tools

--- tools/clean.js
+++ tools/clean.js
@@ -4,14 +4,4 @@
 
 import rimraf from 'rimraf';
 
-function clean(paths) {
-  if (Array.isArray(paths)) {
-    for (const path of paths) {
-      rimraf.sync(path);
-    }
-  } else {
-    rimraf.sync(path);
-  }
-}
-
-clean(['cjs', 'esm', 'types', 'examples/qrcode.js']);
+rimraf.sync(['cjs', 'esm', 'types', 'examples/qrcode.js']);
