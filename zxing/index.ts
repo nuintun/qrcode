2e05690bd6f4882c95f17166650b41d1ce49f489
nuintun <nuintun@qq.com>
2023-06-14 21:25:47 +0800

    refactor: Refactor code

--- zxing/index.ts
+++ zxing/index.ts
@@ -5,6 +5,7 @@
 export { Charset } from './common/Charset';
 export { Encoder } from './encoder/Encoder';
 export { Byte } from './encoder/segments/Byte';
+export { Hanzi } from './encoder/segments/Hanzi';
 export { Kanji } from './encoder/segments/Kanji';
 export { Numeric } from './encoder/segments/Numeric';
 export { Alphanumeric } from './encoder/segments/Alphanumeric';
