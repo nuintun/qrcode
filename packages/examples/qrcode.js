215b4fc39681d14f734f4ea64def86a3683e853f
nuintun <nuintun@qq.com>
2023-11-24 17:09:35 +0800

    feat: Update files

--- packages/examples/qrcode.js
+++ packages/examples/qrcode.js
@@ -12,8 +12,8 @@
   typeof exports === 'object' && typeof module !== 'undefined'
     ? factory(exports)
     : typeof define === 'function' && define.amd
-    ? define('qrcode', ['exports'], factory)
-    : ((global = typeof globalThis !== 'undefined' ? globalThis : global || self), factory((global.QRCode = {})));
+      ? define('qrcode', ['exports'], factory)
+      : ((global = typeof globalThis !== 'undefined' ? globalThis : global || self), factory((global.QRCode = {})));
 })(this, function (exports) {
   'use strict';
 
