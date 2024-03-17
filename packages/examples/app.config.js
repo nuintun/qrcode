4d3f9d148f949bcab29b1d6345ee9c7683f7f994
nuintun <nuintun@qq.com>
2024-03-17 12:39:12 +0800

    feat: Update examples

--- packages/examples/app.config.js
+++ packages/examples/app.config.js
@@ -20,7 +20,7 @@ export default {
   outputPath: path.resolve('static/public'),
   entryHTML: path.resolve('static/index.html'),
   favicon: path.resolve('src/images/favicon.ico'),
-  publicPath: '/qrcode/packages/examples/static/',
   alias: { '/js': js, '/css': css, '/images': images },
+  publicPath: '/qrcode/packages/examples/static/public',
   meta: { viewport: 'width=device-width,initial-scale=1.0' }
 };
