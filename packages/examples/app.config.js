f16fd18fbd07eb3ec3b2cd2d2969c2bc3db3e677
nuintun <nuintun@qq.com>
2024-03-17 12:36:32 +0800

    feat: Update examples

--- packages/examples/app.config.js
+++ packages/examples/app.config.js
@@ -15,12 +15,12 @@ const images = path.resolve('src/images');
 export default {
   name: '二维码',
   lang: 'zh-CN',
-  publicPath: '/public/',
   context: path.resolve('src'),
   entry: [path.resolve('src/js/index.tsx')],
   outputPath: path.resolve('static/public'),
   entryHTML: path.resolve('static/index.html'),
   favicon: path.resolve('src/images/favicon.ico'),
+  publicPath: '/qrcode/packages/examples/static/',
   alias: { '/js': js, '/css': css, '/images': images },
   meta: { viewport: 'width=device-width,initial-scale=1.0' }
 };
