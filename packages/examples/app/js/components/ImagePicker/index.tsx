06245d8bc420bf7bd3e6c9e6f68b49379988a7f4
nuintun <nuintun@qq.com>
2025-04-15 10:28:50 +0800

    feat(ImageUpload): add drag and drop functionality
    - Implement drag and drop feature for image upload
    - Update component to handle dragging state and dropped files
    - Modify styles to show visual feedback for dragging
    - Add error handling for invalid file types

--- packages/examples/app/js/components/ImagePicker/index.tsx
+++ packages/examples/app/js/components/ImagePicker/index.tsx
@@ -1,15 +1,17 @@
 import * as styles from './scss/index.module.scss';
 
 import { App } from 'antd';
+import classNames from 'classnames';
 import { fileOpen } from 'browser-fs-access';
-import React, { memo, useCallback, useRef } from 'react';
 import useControllableValue from '/js/hooks/useControllableValue';
+import React, { memo, useCallback, useRef, useState } from 'react';
 
 const { useApp } = App;
 
+type DivDragEvent = React.DragEvent<HTMLDivElement>;
+
 export interface ImageUploadProps {
   value?: string;
-  disabled?: boolean;
   defaultValue?: string;
   children?: React.ReactNode;
   onChange?: (value: string) => void;
@@ -19,36 +21,90 @@ export interface ImageUploadProps {
 export default memo(function ImageUpload(props: ImageUploadProps) {
   const { message } = useApp();
   const urlRef = useRef<string>();
-  const { children, disabled, preview } = props;
+  const { children, preview } = props;
+  const [dragging, setDragging] = useState(false);
   const [value, setValue] = useControllableValue<string>(props);
 
   const onClick = useCallback(() => {
-    if (!disabled) {
-      fileOpen({ mimeTypes: ['image/*'] }).then(
-        file => {
-          if (/^image\/.+$/i.test(file.type)) {
-            setValue(() => {
-              if (urlRef.current) {
-                URL.revokeObjectURL(urlRef.current);
-              }
+    fileOpen({ mimeTypes: ['image/*'] }).then(
+      file => {
+        if (/^image\/.+$/i.test(file.type)) {
+          setValue(() => {
+            if (urlRef.current) {
+              URL.revokeObjectURL(urlRef.current);
+            }
 
-              urlRef.current = URL.createObjectURL(file);
+            urlRef.current = URL.createObjectURL(file);
 
-              return urlRef.current;
-            });
-          } else {
-            message.error('请选择图片格式文件');
-          }
-        },
-        () => {
-          // 读出文件失败
+            return urlRef.current;
+          });
+        } else {
+          message.error('请选择图片格式文件');
         }
-      );
+      },
+      () => {
+        // 读取文件失败
+        message.error('读取文件失败');
+      }
+    );
+  }, []);
+
+  const onDragOver = useCallback((event: DivDragEvent) => {
+    event.preventDefault();
+
+    setDragging(true);
+  }, []);
+
+  const onDrop = useCallback((event: DivDragEvent) => {
+    event.preventDefault();
+
+    const { files } = event.dataTransfer;
+    const { length } = files;
+
+    let passed = false;
+
+    for (let i = 0; i < length; i++) {
+      const file = files[i];
+
+      if (/^image\/.+$/i.test(file.type)) {
+        setValue(() => {
+          if (urlRef.current) {
+            URL.revokeObjectURL(urlRef.current);
+          }
+
+          urlRef.current = URL.createObjectURL(file);
+
+          return urlRef.current;
+        });
+
+        passed = true;
+
+        break;
+      }
     }
-  }, [disabled]);
+
+    setDragging(false);
+
+    if (!passed) {
+      message.error('请选择图片格式文件');
+    }
+  }, []);
+
+  const onDragLeave = useCallback((event: DivDragEvent) => {
+    event.preventDefault();
+
+    setDragging(false);
+  }, []);
 
   return (
-    <div className={styles.upload}>
+    <div
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      className={classNames(styles.upload, {
+        [styles.dragging]: dragging
+      })}
+    >
       <div onClick={onClick}>{children}</div>
       {preview && preview(value)}
     </div>
